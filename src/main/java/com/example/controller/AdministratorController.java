6e7ff47caa4eda7dd9e9ad2bec05457ae1220e09
NaganoRyotaNV <nagano30123012@gmail.com>
2026-05-20 13:44:48 +0900

    AdministratorControllerの作成

---
+++ src/main/java/com/example/controller/AdministratorController.java
@@ -0,0 +1,22 @@
+package com.example.controller;
+
+import com.example.form.InsertAdministratorForm;
+import com.example.service.AdministratorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+
+public class AdministratorController {
+    @Autowired
+    private AdministratorService administratorService;
+
+    @GetMapping("")
+    public String toInsert(InsertAdministratorForm form) {
+        return "administrator/insert";
+    }
+
+}
