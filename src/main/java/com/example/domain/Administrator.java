4b26643b0a2bab8bb7390b6f5781ac81e74a94b5
NaganoRyotaNV <nagano30123012@gmail.com>
2026-05-20 11:28:06 +0900

    administratorsDBに対応したドメインの定義を完了

---
+++ src/main/java/com/example/domain/Administrator.java
@@ -0,0 +1,22 @@
+package com.example.domain;
+
+import lombok.*;
+
+/**
+ * Administratorドメインの定義.
+ * 管理者情報を示すドメインクラスadministratorsテーブルに対応。
+ */
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+
+public class Administrator {
+    private Integer id;
+    private String name;
+    private String mailAddress;
+    private String password;
+
+}
---
+++ src/main/java/com/example/domain/Employee.java
@@ -0,0 +1,18 @@
+package com.example.domain;
+
+import java.util.Date;
+
+public class Employee {
+    private Integer id;
+    private String name;
+    private String image;
+    private String gender;
+    private Date hireDate;
+    private String mailAddress;
+    private String zipCode;
+    private String telephone;
+    private Integer salary;
+    private String characteristics;
+    private Integer dependentsCount;
+
+}
